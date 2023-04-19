1d914e5e55821d3b11601d7434a05c5c0fc9f02a
FahrezaDaffa93 <117092951+FahrezaDaffa93@users.noreply.github.com>
2023-04-19 15:07:21 +0700

    up db

---
+++ database/migrations/2023_04_19_065129_create_karyawan.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{e
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('karyawan', function (Blueprint $table) {
+            $table->id('id_karyawan');
+            $table->string('nama_karyawan');
+            $table->string('alamat');
+            $table->string('no_telpon');
+            $table->string('email');
+            $table->string('posisi');
+            $table->string('Gaji');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('karyawan');
+    }
+};

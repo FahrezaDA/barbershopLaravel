4cb50ed22875a982fc77822326c41c8843985369
Khoiyimatul Hosniah <khoiyimatulhosniah23@gmail.com>
2023-05-07 17:15:24 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [LoginController::class, 'Login']);
 
-Route::get('/', function () {
-    return view('welcome');
-});

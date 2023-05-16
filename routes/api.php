83019949d3ec15839c9e45bbec54abef15f76805
FahrezaDaffa93 <117092951+FahrezaDaffa93@users.noreply.github.com>
2023-05-16 14:11:33 +0700

    tes

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,11 @@ use App\Http\Controllers\Api\UserController;
 //    return $request->user();
 //});
 
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+   return $request->user();});
 
 Route::post('login', [UserController::class,'login']);
 Route::post('loginUser', [UserController::class,'user']);

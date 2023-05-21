c88faaeb81c454caf5e6025ba52601afe97265de
developer <sucinurkholisah15@gmail.com>
2023-05-21 14:53:52 +0700

    Create pengeluaranController.php

---
+++ app/Http/Controllers/pengeluaranController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\pengeluaran;
+
+class pengeluaranController extends Controller
+{
+    public function postPengeluaran(Request $request) {
+        $pengeluaran = new Pengeluaran();
+        $pengeluaran->jenis_pengeluaran = $request->jenis_pengeluaran;
+        $pengeluaran->id_fasilitas = $request->id_fasilitas;
+        $pengeluaran->jumlah = $request->jumlah;
+        $pengeluaran->biaya = $request->biaya;
+        $pengeluaran->bukti_nota = $noAntrian;
+        $pengeluaran->tanggal_pengeluaran = $tanggalPengeluaran;
+        $pengeluaran->id_kasir = $request->id_kasir;
+        $pengeluaran->save();
+
+        return response()->json([
+            "message" => "Success",
+            "data" => $pemesanan
+        ]);
+    }
+
+
+}

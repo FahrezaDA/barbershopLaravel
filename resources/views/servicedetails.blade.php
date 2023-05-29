cffc005ac9118aeff3942d79cc7e0e1eb4a238b7
developer <sucinurkholisah15@gmail.com>
2023-05-29 21:29:52 +0700

    Create servicedetails.blade.php

---
+++ resources/views/servicedetails.blade.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Service Details</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="{{ asset('image/iconatas.png') }}" rel="icon">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Roboto:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Work+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  {{-- <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet"> --}}
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: UpConstruction - v1.2.1
+  * Template URL: https://bootstrapmade.com/upconstruction-bootstrap-construction-website-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header d-flex align-items-center">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+      <div id="logo">
+        <a href="index.php"><img src="assets/img/projects/gambar.png" alt="image" height="60" width="200"></a>
+        </div>
+
+      <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
+      <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
+      <nav id="navbar" class="navbar">
+        <ul>
+            <li><a href="/">Utama</a></li>
+            <li><a href="/pelayanan">Pelayanan </a></li>
+            <li><a href="/tentangkami">Tentang Kami </a></li>
+            <li><a href="/galeri">Galeri </a></li>
+            <li><a href="/kontak">Kontak</a></li>
+            <li><a href="dashboardPemesanan.php">Dashboard </a></li>
+            <li><a href="/login">Login</a></li>
+        </ul>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <div class="breadcrumbs d-flex align-items-center" style="background-image: url('gambar/background\ \(4\).jpg');">
+      <div class="container position-relative d-flex flex-column align-items-center" data-aos="fade">
+
+        <h2>Pelayanan Detail</h2>
+        <ol>
+          <li><a href="/">Utama</a></li>
+          <li>Pelayanan Detail</li>
+        </ol>
+
+      </div>
+    </div><!-- End Breadcrumbs -->
+
+    <!-- ======= Service Details Section ======= -->
+    <section id="portfolio-details" class="portfolio-details1">
+      <div class="container">
+
+        <div class="row gy-4">
+
+          <div class="col-lg-6">
+            <div class="portfolio-details-slider swiper">
+              <div class="swiper-wrapper align-items-center">
+
+                <div class="swiper-slide">
+                  <img src="Pelayanan/gentlemen-cut2.jpg" alt="" width="350">
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-4">
+            <div class="portfolio-info">
+              <h3>Project information</h3>
+              <ul>
+                <li><strong>Kategori </strong> : <br>dewasa</li><br>
+                <li><strong>Model </strong> :<br>gentlemen cut</li>
+                <br>
+                <li><strong>Harga </strong> :<br>Rp 25.000 - Rp 35.000</li>
+                <br>
+                <li><strong>Deskripsi </strong> : <br>
+                  <p>Sesuai namanya, gaya ini dapat memberikan tampilan rambut pria yang berkesan gentleman dan mempesona.
+                     Potongan rambut tipis berlapis pada sisi bawah dan samping kepala akan memamerkan tekstur rambut bergelombang
+                     di bagian atas yang indah. Gaya ini tentu memberikan pesona pria yang menarik hingga mudah mendapat perhatian wanita.
+                  </p>
+                  <br>
+                  <button onclick="window.location='http://localhost/php/repo-barbershop/barbershopPro/booking.php' "style="background-color: #4E73DF; color: white; height: 35px; width: 150px;">
+                  <b>Booking</b>
+                  </button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+    <!-- End Service Details Section -->
+
+
+        </div>
+
+      </div>
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h3>CutLuck</h3> <br><br>
+            <p>
+              Jl.Mastrip,  Krajan Timur <br>
+                Kec.Sumbersari, Jember <br>
+                Jawa Timur, 68121<br><br>
+            </p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Halaman</h4>
+            <ul>
+              <li><i class="bi bi-chevron-right"></i> <a href="index.php">Utama</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="pelayanan.php">Pelayanan</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="tentangKami.php">Tentang Kami</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="projects.php">Galeri</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="kontak.php">Kontak</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Tim Kami</h4>
+            <ul>
+              <li> Fahreza Daffa Assefy </li>
+              <li> Lucki Wahyu M</li>
+              <li> Khoiyimatul Hosniah</li>
+              <li> Retno Ayu Nawang W</li>
+              <li> Suci Nur Kholisah</li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Sosial Media</h4>
+            <p>Anda dapat menghubungi kami dengan</p>
+            <div class="social-links mt-3">
+              <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+              <a href="#" class="whatsapp"><i class="bi bi-whatsapp"></i></a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container py-4">
+      <div class="copyright">
+        &copy; Copyright <strong><span>CutLuck BarberShop</span></strong>. Manajemen Informatika
+      </div>
+
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <div id="preloader"></div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+
+</body>
+
+</html>

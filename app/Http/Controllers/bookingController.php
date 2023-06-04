0e20c2e4c61acecfe0a1cc331583176fc0fde27b
Khoiyimatul Hosniah <khoiyimatulhosniah23@gmail.com>
2023-06-04 23:51:53 +0700

    Update bookingController.php

--- app/Http/Controllers/bookingController.php
+++ app/Http/Controllers/bookingController.php
@@ -6,43 +6,79 @@ use Illuminate\Http\Request;
 
 class bookingController extends Controller
 {
-
-    public function booking() {
-        return view ('bookingCustomer');
-    }
-
-    public function getBooking()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function booking()
     {
-        $data = booking::select('id_booking', 'jenis_pelayanan', 'tanggal_booking', 'jam_booking', 'stats')->get();
-        return response()->json([
-            "message" => "Success",
-            "data"=> $data
-        ]);
+        return view('booking');
     }
 
-
-    public function postBooking(Request $request){
-
-      // Membuat objek Pemesanan baru
-      $booking = new Booking();
-      $booking->id_booking = $request->id_booking;
-      $booking->id_user = $request->id_user;
-      $booking->id_karyawan = $request->id_karyawan;
-      $booking->nama = $request->nama_customer;
-      $booking->no_telpon = $request->no_telpon;
-      $booking->jenis_pelayanan = $request->jenis_pelayanan;
-      $booking->harga = $request->harga;
-      $booking->tanggal_booking = $request -> $tanggal_booking;
-      $booking->jam_booking = $request -> $jam_booking;
-      $booking->bukti_transfer = $request -> $buktiTransfer;
-      $booking->stats = $request -> $stats;
-      $booking->save();
-
-      return response()->json([
-          "message" => "Success",
-          "data" => $pemesanan
-      ]);
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }

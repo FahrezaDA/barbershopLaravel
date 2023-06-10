9201400791673566b6fa94ef100279bcf2e2e223
Khoiyimatul Hosniah <khoiyimatulhosniah23@gmail.com>
2023-06-10 15:49:24 +0700

    Update pengeluaran.blade.php

--- resources/views/pengeluaran.blade.php
+++ resources/views/pengeluaran.blade.php
@@ -51,74 +51,76 @@ if( isset($_POST['submit']) ){
 
 <body class="bg-gradient-primary">
 
-    <div class="container">
+@php
+    $fasilitas = \App\Models\Fasilitas::all();
+    $kasir = \App\Models\Kasir::all();
+@endphp
 
-        <div class="card o-hidden border-0 shadow-lg" style="margin-top: 107px;">
-            <div class="card-body p-0">
-                <!-- Nested Row within Card Body -->
-                <div class="row">
-                    <div class="col-lg-5 d-none d-lg-block bg-logo"></div>
-                    <div class="col-lg-7 bg-form">
-                        <div class="p-4">
-                            <div class="text-center">
-                                <h1 class="h4 text-gray-900 mb-4">TAMBAHKAN PENGELUARAN</h1>
+<div class="container">
+    <div class="card o-hidden border-0 shadow-lg" style="margin-top: 107px;">
+        <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+                <div class="col-lg-5 d-none d-lg-block bg-logo"></div>
+                <div class="col-lg-7 bg-form">
+                    <div class="p-4">
+                        <div class="text-center">
+                            <h1 class="h4 text-gray-900 mb-4">TAMBAHKAN PENGELUARAN</h1>
+                        </div>
+                        <form class="user" action="{{ route('pengeluaran.create') }}" method="POST"
+                            enctype="multipart/form-data">
+                            @csrf
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-user" id="exampleInputUsername"
+                                    placeholder="Jenis Pengeluaran" name="txt_jenis_pengeluaran">
                             </div>
-                            <form class="user" action="/pengeluaran" method="POST" enctype="multipart/form-data">
-                                <div class="form-group">
-                                    <input type="text" class="form-control form-control-user" id="exampleInputUsername"
-                                        placeholder="Jenis Pengeluaran" name="txt_jenis_pengeluaran">
-                                </div>
-                                <div class="form-group">
-                                <select type="text" placeholder="Pilih Daftar Sebagai" class="form-control  form-select" name="txt_id_fasilitas" id="OptionLevel">
-                                <option>Nama Fasilitas</option>
-                                 <?php
-                                 /*
-                                $query = "SELECT * FROM fasilitas";
-                                $result = mysqli_query($koneksi, $query);
-                                while ($row = mysqli_fetch_array($result)) {
-                                echo "<option value=$row[id_fasilitas] > $row[nama_fasilitas] </option>";}
-                                */
-                                ?>
+                            <div class="form-group">
+                                <select class="form-control form-select" name="txt_id_fasilitas" id="OptionLevel">
+                                    <option>Nama Fasilitas</option>
+                                    @foreach($fasilitas as $item)
+                                        <option value="{{ $item->id_fasilitas }}">{{ $item->nama_fasilitas }}</option>
+                                    @endforeach
                                 </select>
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" class="form-control form-control-user" id="exampleInputUsername"
-                                        placeholder="Jumlah" name="txt_jumlah">
-                                </div>
-                                <div class="form-group">
-                                    <input type="text" class="form-control form-control-user" id="exampleInputUsername"
-                                        placeholder="biaya " name="txt_biaya">
-                                </div>
-                                <div class="form-group">
-                                    <p><b>Bukti Pembayaran :</P>
-                                    <input  placeholder="bukti transfer" type="file" class="form-control form-select" id="exampleInputUsername"
-                                        name="bukti_nota">
-                                </div>
-                                <div class="form-group">
-                                    <input type="date" class="form-control form-control-user" 
-                                        placeholder="Tanggal Pengeluaran" name="txt_tanggal_pengeluaran">
-                                </div>
-                                <div class="form-group">
-                                <select type="text" placeholder="Pilih Kasir" class="form-control  form-select" name="txt_id_kasir" id="OptionLevel">
-                                <option>Pilih Kasir</option>
-                                 <?php
-                                 /*
-                                $query = "SELECT * FROM kasir";
-                                $result = mysqli_query($koneksi, $query);
-                                while ($row = mysqli_fetch_array($result)) {
-                                echo "<option value=$row[kasirID] > $row[username] </option>";}
-                                */
-                                ?>
-                                </select>
-                                </div>
-                                <button type="submit" name="submit" class="btn btn-primary btn-user btn-block">Tambah Pengeluaran</button>
-                            </form>
-                            <hr>
-                            <div class="text-center">
-                                <a class="small" href="/login">Already have an account? Login!</a>
                             </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-user" id="exampleInputUsername"
+                                    placeholder="Jumlah" name="txt_jumlah">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control form-control-user" id="exampleInputUsername"
+                                    placeholder="biaya" name="txt_biaya">
+                            </div>
+                            <div class="form-group">
+                                <p><b>Bukti Pembayaran :</p>
+                                <input placeholder="bukti transfer" type="file"
+                                    class="form-control form-select" id="exampleInputUsername" name="bukti_nota">
+                            </div>
+                            <div class="form-group">
+                                <input type="date" class="form-control form-control-user"
+                                    placeholder="Tanggal Pengeluaran" name="txt_tanggal_pengeluaran">
+                            </div>
+                            <div class="form-group">
+                                <select class="form-control form-select" name="txt_id_kasir" id="OptionLevel">
+                                    <option>Pilih Kasir</option>
+                                    @foreach($kasir as $item)
+                                        <option value="{{ $item->kasirID }}">{{ $item->username }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <button type="submit" name="submit"
+                                class="btn btn-primary btn-user btn-block">Tambah Pengeluaran</button>
+                        </form>
+                        <hr>
+                        <div class="text-center">
+                            <a class="small" href="/login">Already have an account? Login!</a>
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
                 </div>
             </div>
         </div>

7a2a69bfa5bf83e0d0839f17d0dd5891d19b29b2
Khoiyimatul Hosniah <khoiyimatulhosniah23@gmail.com>
2023-06-10 15:51:07 +0700

    Update pengeluaran.php

--- app/Models/pengeluaran.php
+++ app/Models/pengeluaran.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class pengeluaran extends Model
 {
     use HasFactory;
+
+    protected $table = 'pengeluaran'; // tabel yang digunakan 
+    protected $primaryKey = 'id_pengeluaran'; 
+
+    
+    protected $fillable = [
+        'jenis_pengeluaran',
+        'id_fasilitas',
+        'jumlah',
+        'biaya',
+        'bukti_nota',
+        'tanggal_pengeluaran',
+        'id_kasir'
+    ];
 }

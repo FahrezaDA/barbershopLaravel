da37a521422a95b93ae51e5e0fad805c51d7518d
Khoiyimatul Hosniah <khoiyimatulhosniah23@gmail.com>
2023-06-13 23:49:55 +0700

    Update landingPage.blade.php

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -58,7 +58,7 @@
           <li><a href="/tentangkami">Tentang Kami </a></li>
           <li><a href="/galeri">Galeri </a></li>
           <li><a href="/kontak">Kontak</a></li>
-          <li><a href="/pemesanan">Dashboard</a></li>
+          <li><a href="/login">Dashboard</a></li>
           <li><a href="/login">Login</a></li>
         </ul>
       </nav><!-- .navbar -->
